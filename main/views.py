4d5e0ff1324a05e59b131da8dc519794f49f2915
charlowit <cargalvezp@gmail.com>
2020-05-02 11:54:20 +0200

    Migrations, define models and urls

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item, ToDoList
 
 
-def index(request):
-    return HttpResponse("<h1>Desarrollado por @Charlowit!</h1>")
+def index(request, name):
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1></br><p>%s</p>" %(ls.name, str(item.text)))
 
-def v1(request):
-    return HttpResponse("<h1>Segunda vista desarrollada por @Charlowit!</h1>")
