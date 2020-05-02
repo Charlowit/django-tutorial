5c0396e101ae651589edff17b172ed169c724987
charlowit <cargalvezp@gmail.com>
2020-05-02 14:01:48 +0200

    HTML Templates, base, home and list

--- main/views.py
+++ main/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Item, ToDoList
 
+def index(response, id):
+	ls = ToDoList.objects.get(id=id)
+	return render(response, "main/list.html", {"ls":ls})
 
-def index(request, id):
-    ls = ToDoList.objects.get(id=id)
-    item = ls.item_set.all()
-    return HttpResponse("<h1>%s</h1></br><p>%s</br>%s</p>" %(ls.name, str(item.get(id=1)), str(item.get(id=2))))
+def home(response):
+	return render(response, "main/home.html", {})
