e0a753e94051eafd234917e86b910a8a52e0b63a
charlowit <cargalvezp@gmail.com>
2020-05-04 16:41:11 +0200

    Formularios

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("<int:id>", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("home/", views.home, name="home"),
     path("", views.home, name="home")
 ]
