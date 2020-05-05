e330ad6f62c0df3788f0ec5500a47ca8d3ebbb36
charlowit <cargalvezp@gmail.com>
2020-05-06 00:44:06 +0200

    Adding bootstrap, some css style, sidevar and some fixes

--- main/views.py
+++ main/views.py
@@ -4,11 +4,29 @@ from .models import Item, ToDoList
 from .forms import CreateNewList
 
 def index(response, id):
-	ls = ToDoList.objects.get(id=id)
-	return render(response, "main/list.html", {"ls":ls})
+    ls = ToDoList.objects.get(id=id)
+    if response.method == "POST":
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("Texto invalido")
+
+    return render(response, "main/list.html", {"ls":ls})
 
 def home(response):
-	return render(response, "main/home.html", {})
+    return render(response, "main/home.html", {})
 
 def create(response):
     if response.method == "POST":
