a55928b46e0dde2885e6dc5977cba621d7520ef0
charlowit <cargalvezp@gmail.com>
2020-05-06 03:07:19 +0200

    Add Login/Logout

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("register/", v.register, name="register"),
     path('', include("main.urls")),
+    path('', include("django.contrib.auth.urls")),
 ]
+
+# start
